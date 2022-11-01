3a70f7c82c928fc738bc6bf44090ff9364bf1ae3
root <sbhargav896@gmail.com>
2022-11-01 15:27:19 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+import BlogList from '../BlogList'
+
+const Home = () => (
+  <div className="home-container">
+    Render UserInfo and BlogList
+    <BlogList />
+  </div>
+)
+
+export default Home
